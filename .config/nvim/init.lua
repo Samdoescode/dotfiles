cbf750baaa6b088e23a4ced7848b111ed8443cae
Sam <kennedyhine@gmail.com>
2022-12-06 17:41:34 +1100

    n

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,17 +29,14 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
+do return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
+
   if packer_bootstrap then
     require('packer').sync()
   end
-end)
+end) end
+
 
 local keymap = function(tbl)
 	-- Some sane default options
