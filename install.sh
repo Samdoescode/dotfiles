ff751396d15beba15d80f4158cb76f57c87e9fc5
Sam <kennedyhine@gmail.com>
2022-12-06 17:07:20 +1100

    with packer

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-# set -x # DEBUG
+#forked originally from https://github.com/jscheytt/dotfiles/blob/main/install.sh &&
+#https://github.com/bashbunni/dotfiles
+
+
 
 ### This is the installation script for Gitpod.
 ### It has a hard timeout of 2mins, so it's a very stripped down version of the
 ### normal installation.
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Install NeoVim
 brew install neovim
